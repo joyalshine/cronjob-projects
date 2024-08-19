e7c1c5a2aaef58cedc231ba2c5590f2d747d979e
Joyal Shine <101260206+joyalshine@users.noreply.github.com>
2024-08-20 01:44:52 +0530

    cron done for resumit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const cron = require('node-cron')
+const axios = require('axios')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send({ Status: 'Server is Running' });
 });
+
+const cronCall = () => {
+  axios
+  .get("https://resumit-combined.onrender.com/cronjob")
+  .then(function (response) {
+    console.log(response.data);
+  });
+}
+
+cron.schedule('*/4 * * * *', cronCall)
 
 module.exports = router;
